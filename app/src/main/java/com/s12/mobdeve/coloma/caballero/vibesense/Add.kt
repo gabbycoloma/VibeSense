8ce5ca63fdf19496ec1fd74966cb4a8ab67e53f4
djdcaballero <don_caballero@dlsu.edu.ph>
2023-03-29 00:33:36 +0800

    Edited fragment_add

--- app/src/main/java/com/s12/mobdeve/coloma/caballero/vibesense/Add.kt
+++ app/src/main/java/com/s12/mobdeve/coloma/caballero/vibesense/Add.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.s12.mobdeve.coloma.caballero.vibesense.databinding.ActivityLandingBinding
+import com.s12.mobdeve.coloma.caballero.vibesense.databinding.FragmentAddBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+private lateinit var binding: FragmentAddBinding
 
 /**
  * A simple [Fragment] subclass.
